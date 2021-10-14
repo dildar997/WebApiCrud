527d8c3a66e248228d8de74bfb2b744a95bcd2dc
Dildar Hussain <92290496+dildar997@users.noreply.github.com>
2021-10-14 14:47:24 +0500

    autoface (for dependecncy injection) and mapping profiles (for automapper) been added and configured

---
+++ WebApiApplicationCrud/WebApiApplicationCrud/AotofacMappingProfiles/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApiApplicationCrud.Models;
+using WebApiApplicationCrud.ViewModels;
+
+namespace WebApiApplicationCrud.AotofacMappingProfiles
+{
+	public class MappingProfiles : Profile
+	{
+		public MappingProfiles()
+		{
+			CreateMap<Country, CountryVM>().ReverseMap();
+			CreateMap<State, StateVM>().ReverseMap();
+			CreateMap<City, CityVM>().ReverseMap();
+
+		}
+	}
+}
---
+++ WebApiApplicationCrud/WebApiApplicationCrud/App_Start/AutofacConfig.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using WebApiApplicationCrud.AotofacMappingProfiles;
+using WebApiApplicationCrud.ServiceRepositories;
+
+namespace WebApiApplicationCrud.App_Start
+{
+	public class AutofacConfig
+	{
+		public static void Register()
+		{
+			var builder = new ContainerBuilder();
+			var config = GlobalConfiguration.Configuration;
+			builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+			RegisterServices(builder);
+			builder.RegisterWebApiFilterProvider(config);
+			builder.RegisterWebApiModelBinderProvider();
+			var container = builder.Build();
+			config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+		}
+
+		public static void RegisterServices(ContainerBuilder builder)
+		{
+			//for auto mapping profile
+			var config = new MapperConfiguration(cgf =>
+			{
+				cgf.AddProfile(new MappingProfiles());
+			});
+			builder.RegisterInstance(config.CreateMapper()).As<IMapper>().SingleInstance();
+
+			//Dependency Injection Repositories
+			builder.RegisterType<CountryRepository>().As<ICountryRepository>().InstancePerRequest();
+			builder.RegisterType<StateRepository>().As<IStateRepository>().InstancePerRequest();
+			builder.RegisterType<CityRepository>().As<ICityRepository>().InstancePerRequest();
+
+
+
+		}
+	}
+}
