4523d812f2da58da3448927526ed268a3c15c811
Dildar Hussain <92290496+dildar997@users.noreply.github.com>
2021-10-14 14:54:51 +0500

    error solved: autofac was not register at global level

--- WebApiApplicationCrud/WebApiApplicationCrud/Global.asax.cs
+++ WebApiApplicationCrud/WebApiApplicationCrud/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebApiApplicationCrud.App_Start;
 
 namespace WebApiApplicationCrud
 {
@@ -18,6 +19,7 @@ namespace WebApiApplicationCrud
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			AutofacConfig.Register();
 		}
 	}
 }
